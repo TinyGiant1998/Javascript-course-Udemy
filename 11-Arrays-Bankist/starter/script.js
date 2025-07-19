94385f312271be72e2e84a7b3abe6d0d4bf482da
Suraj Budhathoki <tinysuraj143@gmail.com>
2025-07-19 18:22:18 +1000

    Adding latest code July 2025

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -6,10 +6,20 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: 'Suraj Budhathoki',
+  movements: [2000000, 600, 450, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+  ],
 };
 
 const account2 = {
@@ -17,6 +27,16 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+  ],
 };
 
 const account3 = {
@@ -24,6 +44,16 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+  ],
 };
 
 const account4 = {
@@ -31,6 +61,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23: 36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -61,16 +103,456 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+// displayMovements(account1.movements);
+
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance.toFixed(2)} €`;
+};
+
+// calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} €`;
+
+  const intrest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${intrest.toFixed(2)} €`;
+};
+// calcDisplaySummary(account1.movements);
+
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+createUsername(accounts);
+
+const updateUI = function (acc) {
+  displayMovements(acc);
+  calcDisplayBalance(acc);
+  calcDisplaySummary(acc);
+};
+
+//Logout timer
+
+const startLogoutTimer = function () {
+  let time = 30;
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    labelTimer.textContent = `${min}:${sec}`;
+
+    if (time == 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+
+      containerApp.style.opacity = 0;
+    }
+    time--;
+  };
+
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
+
+//Event Handler for login
+
+let currentAccount, timer;
+
+// Fake login
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+  }
+  containerApp.style.opacity = 100;
+
+  //currentDate
+  const now = new Date();
+  const day = `${now.getDate()}`.padStart(2, 0);
+  const month = `${now.getMonth() + 1}`.padStart(2, 0);
+  const year = now.getFullYear();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+  //Clear the input field
+  inputLoginUsername.value = inputLoginPin.value = '';
+  inputLoginPin.blur();
+  if (timer) clearInterval(timer);
+  timer = startLogoutTimer();
+  updateUI(currentAccount);
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(+amount);
+
+    //Add Transfer date
+    currentAccount.movementsDates.push(new Date());
+    receiverAcc.movementsDates.push(new Date());
+    updateUI(currentAccount);
+    clearInterval(timer);
+    timer = startLogoutTimer();
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Math.floor(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= 0.1 * amount)) {
+    currentAccount.movements.push(amount);
+
+    //Add Transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+
+    updateUI(currentAccount);
+    clearInterval(timer);
+    timer = startLogoutTimer();
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount, !sorted);
+  sorted = !sorted;
+});
+
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+// console.log(account2.movements.every(mov => mov > 0));
+
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = [...document.querySelectorAll('.movements_value')];
+  console.log(movementsUI.map(el => el.textContent.replace('€', '')));
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+console.log(23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(3 / 10);
+console.log(Number('23'));
+console.log('23' + 23);
+console.log(Number.parseInt('03px', 10));
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseFloat('    2.5rem'));
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(Number.isNaN(20));
+console.log(Number.isFinite(-100));
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(1, 6));
+let value = 1;
+let result = value || 'default';
+console.log(result);
+const isEven = n => (n % 2 === 0 ? 'even' : 'false');
+console.log(isEven(513));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements_row')].forEach(function (row, i) {
+    if (i % 2 === 0)
+      row.style.backgroundColor = 'orangered'
+  })
+});
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// currencies.forEach(function (value, key, map) {
+// console.log(`${key}: ${value}`);
+// })
+
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+
+// currenciesUnique.forEach(function (value, key, set) {
+//   console.log(`${key}: ${value}`);
+
+// })
+
+// for (const movement of movements) {
+// for (const [i, movement] of movements.entries()){
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdraw ${Math.abs(movement)}`);
+//   }
+// }
+// console.log('--- ForEach ---');
+// movements.forEach(function (mov, i, arr) {
+//  if (mov > 0) {
+//    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//  } else {
+//    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
+//  }
+// });
 
 /////////////////////////////////////////////////
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log([...arr]);
+
+// console.log(arr.splice(2));
+// console.log(arr);
+
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// console.log(letters.join(' - '));
+
+// const arr = [23, 11, 64]
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+// console.log(arr.at(-3));
+
+// console.log('Suraj'.at(-1));
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+//   console.log(dogsJuliaCorrected);
+//   const arr = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(arr);
+
+//   arr.forEach(function (dogs, i) {
+//     if (dogs >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dogs} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy`);
+//     }
+//   });
+// };
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter(age => age >= 18);
+//   console.log(humanAges);
+//   console.log(adults);
+
+//   // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   const average = adults.reduce(
+//     (acc, age, i, arr) => acc + age / arr.length,
+//     0
+//   );
+//   //
+//   return average;
+// };
+
+// const average2 = ages =>
+//   ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+// const avg3 = average2([5, 2, 4, 1, 15, 8, 3]);
+// const avg4 = average2([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg3, avg4);
+
+// const movements = [100, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementsUSDfor = [];
+// for (const mov of movements) {
+//   movementsUSDfor.push(mov * eurToUsd);
+// }
+// console.log(movementsUSDfor);
+
+// const movementsDescription = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
+
+// console.log(movementsDescription);
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+
+// console.log(withdrawals);
+
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 1);
+// console.log(balance);
+
+// let balance2 = 0;
+// for (const mov of movements) {
+//   balance2 += mov;
+// }
+// console.log(balance2);
+
+// //maximum value of movements
+
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+// console.log(max);
+
+// const totalDepositsUSD = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * eurToUsd)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDepositsUSD);
+
+// let result111 = '5' + 2 - 1;
+// console.log(result111);
+// console.log(typeof result111);
+
+// const firstWithdrawal = movements.find(mov => mov < 0);
+// console.log(movements);
+// console.log(firstWithdrawal);
+// console.log(accounts);
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+// console.log(account);
+
+// const account12 = [];
+// for (const acc of accounts)
+//   if (acc.owner === 'Jessica Davis') account12.push(acc);
+// console.log(...account12);
+
+// console.log('Suraj');
+
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,418 @@
-'use strict';
+// 'use strict';
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+//   // Never do this
+//   // this.calcAge = function () {
+//   //   console.log(2025 - birthYear);
+//   // };
+// };
+
+// const suraj = new Person('Suraj', 1998);
+// console.log(suraj);
+
+// const surya = new Person('Surya', 1997);
+// console.log(surya, suraj);
+
+// //Prototypes
+// console.log(Person.prototype);
+
+// Person.prototype.calcAge = function () {
+//   console.log(2025 - this.birthYear);
+// };
+
+// suraj.calcAge();
+
+// console.log(suraj.__proto__);
+// console.log(suraj.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(suraj));
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(suraj);
+// console.log(suraj.species);
+
+// console.log(suraj.hasOwnProperty('firstName'));
+// console.log(suraj.hasOwnProperty('species'));
+
+// const arr = [1, 2, 3, 4, 5, 1, 2, 5];
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// const car1 = new Car('BMW', 120);
+// const car2 = new Car('Mercedes', 95);
+
+// Car.prototype.acceleration = function () {
+//   this.speed += 10;
+//   console.log(`The speed of ${this.make} is ${this.speed}.`);
+// };
+
+// //
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`The speed of ${this.make} is ${this.speed}.`);
+// };
+
+// car1.acceleration();
+// car1.acceleration();
+// car1.brake();
+// car1.acceleration();
+
+//ES6 Classes
+
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+
+//   calcAge() {
+//     console.log(2025 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hello ${this.fullName}`);
+//   }
+
+//   get age() {
+//     return 2025 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not full name!`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   //static method
+//   static hey() {
+//     console.log('Hey there 😉');
+//     console.log(this);
+//   }
+// }
+
+// const suraj = new PersonCl('Suraj Budhathoki', 1998);
+// console.log(suraj);
+// suraj.calcAge();
+// console.log(suraj.__proto__ === PersonCl.prototype);
+// suraj.greet();
+// console.log(suraj.age);
+// PersonCl.hey();
+
+// const walter = new PersonCl('Walter White', 1997);
+
+// const account = {
+//   owner: 'suraj',
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   },
+// };
+
+// console.log(account.latest);
+// account.latest = 50;
+// console.log(account.movements);
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2025 - this.birthYear);
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+// steven.name = 'Steven';
+// steven.birthYear = 1990;
+// steven.calcAge();
+
+// console.log(steven.__proto__ === PersonProto);
+
+// const sarah = Object.create(PersonProto);
+// sarah.init('Sarah', 1988);
+// sarah.calcAge();
+// console.log(sarah);
+
+// //Challenge #02
+
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`The speed of ${this.make} is ${this.speed} km/hr.`);
+//   }
+
+//   brake() {
+//     this.speed -= 5;
+//     console.log(`The speed of ${this.make} is ${this.speed} km/hr.`);
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+// const ford = new CarCl('Ford', 120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+
+// ford.speedUS = 50;
+// console.log(ford);
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(2025 - this.birthYear);
+// };
+
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
+
+// const suraj = new Student('Suraj', 1998, 'BIT');
+// console.log(suraj);
+// suraj.introduce();
+// suraj.calcAge();
+
+// //Challenge #02
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.acclerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is going at ${this.speed}km/h`);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed}km/h`);
+// };
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = chargeTo;
+// };
+
+// EV.prototype.acclerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(
+//     `${this.make} is going at ${this.speed}km/h and charge is ${this.charge}`
+//   );
+// };
+
+// const tesla = new EV('Tesla', 120, 23);
+// tesla.chargeBattery(90);
+// console.log(tesla);
+// tesla.acclerate();
+// tesla.brake();
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+}
+
+const suraj = new StudentCl('Suraj', 1998, 'BIT');
+console.log(suraj);
+suraj.introduce();
+
+const PersonProto = {
+  calacAge() {
+    console.log(2025 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'CS');
+console.log(jay);
+jay.introduce();
+jay.calacAge();
+
+// 1. Public Fields
+// 2. Private Fields
+// 3. Public methods
+// 4. Private methods
+
+class Account {
+  locale = navigator.language;
+  bank = 'Bankist';
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+    // this.movements = [];
+    // this.locale = navigator.language;
+  }
+
+  // Public Interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  #approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+    return this;
+  }
+}
+
+const acc1 = new Account('Suraj', 'EUR', 1111);
+// acc1.deposit(250);
+// acc1.withdraw(140);
+// acc1.requestLoan(1000);
+// console.log(acc1);
+
+acc1.deposit(300).withdraw(100).withdraw(50).requestLoan(25000).withdraw(4000);
+console.log(acc1);
+
+//challenge #04
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`The speed of ${this.make} is ${this.speed} km/hr.`);
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`The speed of ${this.make} is ${this.speed} km/hr.`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed}km/h and charge is ${this.#charge}`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+rivian.accelerate().accelerate().brake().chargeBattery(55).accelerate();
+
+console.log(rivian.speedUS);
